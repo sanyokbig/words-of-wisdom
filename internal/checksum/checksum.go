8566c93fac3c8014f7c45ebb8f503aea9caa1de3
sanyokbig <alexander.gusev@hey.com>
2022-03-20 19:07:20 +0300

    commentary corrected

--- internal/checksum/checksum.go
+++ internal/checksum/checksum.go
@@ -12,7 +12,7 @@ import (
 // 2. Concatenating resulted hexes
 func Make(sequence []uint64) string {
 	// Uint64 consists of 64*8=512 bits. Since hex represents 16 bits, uint64 as hex will have at most 16 hexes.
-	// Since we will be padding hexes with zeros to have a unique checksum, it is safe to assume that we will need
+	// Since we will be padding hexes with zeros to have a unique source string, it is safe to assume that we will need
 	// a number of sequences times 16
 	joinedHexes := make([]byte, 0, len(sequence)*16)
 
